e59235bd538a7498e0f8ccf729f5e835e315fadf
Aktishch <aktishch@gmail.com>
2025-06-09 00:37:37 +0300

    add new pages

--- plugins/animation.js
+++ plugins/animation.js
@@ -9,6 +9,14 @@ module.exports = plugin(
         transitionTimingFunction: 'ease',
       },
     }
+    let clipPath = {
+      '.clip-path': {
+        transitionProperty: 'clip-path',
+        transitionDuration: '300ms',
+        transitionTimingFunction: 'ease',
+        clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%)',
+      },
+    }
     Object.entries(theme('anim')).map(([key, value]) => {
       anim = {
         ...anim,
@@ -19,7 +27,16 @@ module.exports = plugin(
         },
       }
     })
+    Object.entries(theme('clipPath')).map(([key, value]) => {
+      clipPath = {
+        ...clipPath,
+        [`.clip-path-${key}:not([data-anim="show"])`]: {
+          clipPath: `${value}`,
+        },
+      }
+    })
     addComponents(anim)
+    addComponents(clipPath)
   },
   {
     theme: {
@@ -32,6 +49,12 @@ module.exports = plugin(
         down: 'translateY(-3.5rem)',
         left: 'translateX(3.5rem)',
         right: 'translateX(-3.5rem)',
+      },
+      clipPath: {
+        up: 'polygon(0 0, 100% 0, 100% 0, 0 0)',
+        down: 'polygon(0 100%, 100% 100%, 100% 100%, 0 100%)',
+        left: 'polygon(100% 0, 100% 0, 100% 100%, 100% 100%)',
+        right: 'polygon(0 0, 0 0, 0 100%, 0 100%)',
       },
     },
   }
